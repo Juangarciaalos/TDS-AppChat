57142f4975bcd3968e3ed694ea7fe6ed08613930
Juangarciaalos <juangarciaalos2@gmail.com>
2025-05-14 17:47:56 +0200

    Ampliar clase Mensaje

--- appChat/src/main/java/um/tds/clases/Mensaje.java
+++ appChat/src/main/java/um/tds/clases/Mensaje.java
@@ -1,5 +1,78 @@
 package um.tds.clases;
 
-public class Mensaje {
+import java.time.LocalDateTime;
 
+public class Mensaje {
+	private String texto;
+	private LocalDateTime hora;
+	private int emoticono;
+	private int tipo;
+	private Usuario emisor;
+	private Usuario receptor;
+	
+	public Mensaje(String texto, Usuario emisor, Usuario receptor) {
+		this.texto = texto;
+		this.hora = LocalDateTime.now();
+		this.receptor = receptor;
+		this.emisor = emisor;
+		this.emoticono = 0; 
+	}
+	
+	public Mensaje(int emoticono, Usuario emisor, Usuario receptor) {
+		this.texto = "";
+		this.hora = LocalDateTime.now();
+		this.receptor = receptor;
+		this.emisor = emisor;
+		this.emoticono = emoticono;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalDateTime hora) {
+		this.hora = hora;
+	}
+
+	public int getEmoticono() {
+		return emoticono;
+	}
+
+	public void setEmoticono(int emoticono) {
+		this.emoticono = emoticono;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public Usuario getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(Usuario emisor) {
+		this.emisor = emisor;
+	}
+
+	public Usuario getReceptor() {
+		return receptor;
+	}
+
+	public void setReceptor(Usuario receptor) {
+		this.receptor = receptor;
+	}
+	
+	
 }
