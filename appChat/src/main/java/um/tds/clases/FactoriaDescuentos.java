4a34c997ac26bba063d0de4566f99dc4a04627df
Juangarciaalos <juangarciaalos2@gmail.com>
2025-06-08 18:13:27 +0200

    Arreglada FactoriaDescuentos

--- appChat/src/main/java/um/tds/clases/FactoriaDescuentos.java
+++ appChat/src/main/java/um/tds/clases/FactoriaDescuentos.java
@@ -1,18 +1,25 @@
 package um.tds.clases;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public enum FactoriaDescuentos {
 	 INSTANCE;
 	
 	 public static List<Descuento> getDescuento(Usuario usuario) {
 		 
-		 List<Descuento> descuentos = new ArrayList<>();
-		 for (Descuento d : List.of(new DescuentoPorFecha(), new DescuentoPorMensaje(), new DescuentoNulo())) {
-			    if (d.esAplicable(usuario)) descuentos.add(d);
-		 }
+		 List<Descuento> descuentos = new ArrayList<>(
+				 Arrays.asList(
+						 new DescuentoPorFecha(), 
+						 new DescuentoPorMensaje(), 
+						 	new DescuentoNulo()
+				 ));
 		 
-		 return descuentos;
+		 return descuentos.stream()
+				 .filter(d -> d.esAplicable(usuario))
+				 .collect(Collectors.toList());
+		 
 	 }
 }
